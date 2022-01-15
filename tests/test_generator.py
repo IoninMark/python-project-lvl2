ace8b8172376d56fa7abe43a89fc63cb90a14725
Mark Ionin <ioninmark91@gmail.com>
2022-01-15 18:32:13 +0300

    fix test_generator.py

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -3,11 +3,11 @@ from gendiff import generate_diff
 
 def test_generate_diff():
     diff = generate_diff(
-        "/Users/mark/Python/python-project-lvl2/tests/fixtures/file1.json", 
-        "/Users/mark/Python/python-project-lvl2/tests/fixtures/file2.json"
+        "tests/fixtures/file1.json", 
+        "tests/fixtures/file2.json"
         )
     file = open(
-        "/Users/mark/Python/python-project-lvl2/tests/fixtures/res_string.txt"
+        "tests/fixtures/res_string.txt"
         )
     result = file.read()
     assert diff == result
